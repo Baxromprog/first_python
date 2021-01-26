67fba083221ca165635f7c0cd6c614f42493ea3f
Bahrom <mail@mail.ru>
2021-01-26 17:57:21 +0500

    first commit

---
+++ 1-dars.py
@@ -0,0 +1,34 @@
+# a=1566789
+# b=45678
+# c="a+b"
+# print(type(c))
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# a="45678"" "
+# b="salom hammaga"
+# c=a+b
+# print(c)
+
+# a=26
+# b=34
+# c=int(a)+int(b)
+# print("natija :",c)
+
+# a=input("Birinchi son :")
+# b=input("Ikkinchi son :")
+# c=int(a)+int(b)
+# print("Natija:",c)
+
+# narxi=5600.70
+# soni=int(input("nechta:"))
+# natija=narxi*soni
+# print("Natija:",natija)
+
+# a=int(input("a="))
+# b=int(input("b="))
+# c='Birinchi son:',b;
+# b='Ikkinchi son:',a;
+# print(c,"\n",b)
+
---
+++ sariq dev2.py
@@ -0,0 +1,112 @@
+# 1-masala
+# print("5 ning 4-darajasini toping:",5**4)
+# 2 - masala
+# print("22 ni 4 ga bo'lganda qancha qoldiq qoladi?",22%4)
+# 3 - masala
+# print("Tomonlari 125 ga teng kvadratning yuzi:",125**2,"\n","perimetrini toping:",4*125)
+# 4 - masala
+# print("Diametri 12 ga teng bo'lgan doiraning yuzini toping  (π=3.14 deb oling)","S=",(6**2)*3.14)
+# 5 - masala
+# print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)",(6*6+7*7)**(1/2))
+
+                                                # 4-dars
+
+# 1-masala:"Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+# hello="Hello World!"
+# print("\"Hello World!\" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring:",hello)
+
+# 2-masala:xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring,
+# keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+# xabar="Salom Hammaga"
+# print("xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring,","\n",
+#       " keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring:",xabar)
+# xabar="Hello World"
+# print(xabar)
+
+# 3-masala:
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+                                             # 5-dars
+
+# matn="mening 💔 parchalangan 🤣"
+# print(matn)
+
+# ism = "James"
+# familiya = 'Bond'
+# ism_sharif = f"{ism} {familiya}"
+# print(ism_sharif.upper())
+# print(ism_sharif.lower())
+# print(ism_sharif.title())
+# print(ism_sharif.capitalize())
+# print('james bond'.upper())
+
+#1-masala: Quyidagi o'zgaruvchilarni yarating:
+# kocha="Bog'bon"
+# mahalla="Sog'bon"
+# tuman="Bodomzor"
+# viloyat="Samarqand"
+# print(kocha)
+# print(mahalla)
+# print(tuman)
+# print(viloyat)
+
+# 2-masala:Yuqoridagi o'zgaruvchilarni jamlab, quyidagi ko'rinishda konsolga chiqaring:
+# Bog'bon ko'chasi, Sog'bon mahallasi, Bodomzor tumani, Samarqand viloyati
+# print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+#       tuman + " tumani, " + viloyat + " viloyati")
+
+# 3-masala:Yuqoridagi o'zgaruvchilarning (kocha, mahalla, tuman, viloyat) qiymatini' \
+#                      ' foydalanuvchidan so'rang. Va avvalgi mashqni takrorlang.
+# kocha=input("Ko'changiz:".upper())
+# mahalla=input("Mahallangiz:".upper())
+# tuman=input("Tumaningiz:".upper())
+# viloyat=input("Viloyatingiz:".upper())
+# print(kocha.upper() + " ko'chasi, ".upper() + mahalla.upper() + " mahallasi, ".upper() +tuman.upper() + " tumani,".upper() + viloyat.upper() + "viloyati".upper())
+
+# 4-masala:Yuqoridagi matnni konsolga chiqarishda har bir verguldan keyin yangi qatordan yozing
+
+# kocha=input("Ko'changiz:".upper())
+# mahalla=input("Mahallangiz:".upper())
+# tuman=input("Tumaningiz:".upper())
+# viloyat=input("Viloyatingiz:".upper())
+# print(kocha.upper() + " ko'chasi,\n".upper() + mahalla.upper() + " mahallasi,\n".upper() +tuman.upper() +
+#       " tumani,\n".upper() + viloyat.upper() + " viloyati,".upper())
+
+# 5-masala:Yuqoridagi o'zgaruvchilarni f-string yordamida, yangi, manzil deb nomlangan o'zgaruvchiga yuklang
+
+# manzil=f"{kocha} kochasi:,{mahalla}mahallasi:{tuman}tumani:,{viloyat}viloyati:"
+# print(manzil)
+
+# 6-masala:manzilga biz yuqorida o'rgangan title(), upper(), lower() , capitalize() metodlarini qo'llab ko'ring.
+meva="    nok     "
+# print("Men",meva.rstrip(),"yaxshi ko'raman")
+# print("Men",meva.lstrip(),"yaxshi ko'raman")
+# print("Men",meva.strip(),"yaxshi ko'raman")
+# print("Men",meva.title(),"yaxshi ko'raman")
+
+# 7-masala:manzilga biz yuqorida o'rgangan title(), upper(), lower() , capitalize() metodlarini qo'llab ko'ring.
+
+# ism="ilyos"
+# familiya="xoshimov"
+# ism_shrif=f"{ism} {familiya}"
+# print(ism_shrif.upper())
+# print(ism_shrif.title())
+# print(ism_shrif.lower())
+# print(ism_shrif.capitalize())
+
+# 8-masala:Yuqoridagi o'zgaruvchilarning (kocha, mahalla, tuman, viloyat) qiymatini foydalanuvchidan so'rang. Va avvalgi mashqni takrorlang.
+
+# kocha=input("Ko'changiz:".upper())
+# mahalla=input("Mahallangiz:".upper())
+# tuman=input("Tumaningiz:".upper())
+# viloyat=input("Viloyatingiz:".upper())
+# # print(kocha+" ko'changgiz,",mahalla+" mahallangiz,",tuman+" tumaningiz,",viloyat+" viloyatingiz,")
+# print(kocha.upper()+" ko'changgiz,\
+# n".upper(),mahalla.lower()+" mahallangiz,\n".lower(),tuman.title()+" tumaningiz,\n".title()
+#       ,viloyat.capitalize()+" viloyatingiz,".capitalize())
+
+
+
